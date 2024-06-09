354d89113d13d2578d303c8955db6e4bad092288
firefly293 <63930363+firefly293@users.noreply.github.com>
2024-06-09 14:05:40 -0700

    lottie stuff

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import React, { createRef, useRef, useState } from 'react';
-import { StyleSheet, SafeAreaView, Text, View, StatusBar, Button, Image, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, StatusBar, Button, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useEffect } from 'react';
 
 import { CircularProgress, CircularProgressBase } from 'react-native-circular-progress-indicator';
 import LottieView from 'lottie-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import colors from "../config/colors"
-import { storeValue, getValue } from '../utils/AsyncStorageManager';
-import { useEffect } from 'react';
+import { GetFormattedDate } from '../utils/GetFormattedDate';
+import { storeData, getData } from '../utils/AsyncStorageManager';
 
 
 
@@ -16,6 +18,13 @@ function HomeScreen(props) {
     const [goal, setGoal] = useState(64)
     const [currentAdd, setCurrentAdd] = useState(8)
 
+    const confettiRef = useRef(null)
+
+    var triggerConfetti = function() {
+        confettiRef.current?.play(0)
+        console.log("Confetti triggered")
+    }
+
     var clamp = function(n, min, max) {
         if (n < min) return min
         if (n > max) return max
@@ -23,72 +32,85 @@ function HomeScreen(props) {
     }
 
     var mySetWater = function(val) {
+        if (water < goal && val >= goal) triggerConfetti()
         setWater(val)
-        AsyncStorage.setItem("data", JSON.stringify({ water: water, goal: goal }), () => { console.log("stored water value " + val.toString()) })
+        storeData(GetFormattedDate(), JSON.stringify({ water: val, goal: goal, date: GetFormattedDate() }))
+
     }
 
     var mySetGoal = function(val) {
         setGoal(val)
-        AsyncStorage.setItem("data", JSON.stringify({ water: water, goal: goal }))
+        storeData(GetFormattedDate(), JSON.stringify({ water: water, goal: val, date: GetFormattedDate() }))
+    }
+
+    var dataCallback = function(dataString) {
+        console.log("in data callback")
+        if (dataString) {
+            var data = JSON.parse(dataString)
+            setWater(clamp(data.water, 0, 1000))
+            setGoal(clamp(data.goal, 0, 1000))
+            console.log("loaded data from disk: water: " + data.water.toString(), " goal: " + data.goal.toString())
+        }
+        else {
+            console.log("no data found")
+        }
     }
 
     useEffect(() => {
         // Set the values to the values in the data
-        var dataCallback = function(dataString) {
-            console.log("in data callback")
-            if (dataString) {
-                var data = JSON.parse(dataString)
-                setWater(clamp(data.water, 0, 1000))
-                setGoal(clamp(data.goal, 0, 1000))
-                console.log("loaded data from disk")
-            }
-            else {
-                console.log("no data found")
-            }
-        }
-
-        AsyncStorage.getItem("data", dataCallback)
+        getData(GetFormattedDate())
+            .then(dataCallback)
     }, [])
 
 
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.topBar} >
-                <Text style={styles.topBarText}>Hydro<Text style={{ color: colors.secondary, fontStyle: "italic" }}>Mate</Text></Text>
-            </View>
-            <View style={styles.mainContent}>
-                <View style={styles.progressSection} elevation={5}>
-                    <Text style={styles.headingText}>Progress</Text>
-                    <CircularProgressBase
-                        value={clamp(water / goal * 100, 0, 100)}
-                        radius={120}
-                        activeStrokeColor={colors.primary}
-                        inActiveStrokeColor={colors.primary}
-                        activeStrokeWidth={30}
-                        inActiveStrokeWidth={30}
-                        inActiveStrokeOpacity={0.2}
-                    >
-                        <Text style={styles.ProgressNumberText}>{water} oz / {goal} oz</Text>
-                    </CircularProgressBase>
-                    <View style={styles.amountToAddAdjustView}>
-                        <TouchableOpacity onPress={() => setCurrentAdd(clamp(currentAdd - 0.5, 0.5, 100))} onLongPress={() => setCurrentAdd(clamp(currentAdd - 10, 0.5, 100))}>
-                            <Image style={styles.addSubtractIcon} source={require("../assets/images/minus.png")}></Image>
-                        </TouchableOpacity>
-                        <Text style={styles.currentAddText}>{currentAdd} oz</Text>
-                        <TouchableOpacity onPress={() => setCurrentAdd(clamp(currentAdd + 0.5, 0.5, 100))} onLongPress={() => setCurrentAdd(clamp(currentAdd + 10, 0.5, 100))}>
-                            <Image style={styles.addSubtractIcon} source={require("../assets/images/plus.png")}></Image>
-                        </TouchableOpacity>
-                    </View>
-
-                    <TouchableOpacity   >
-                        <Button title="LOG" onPress={() => mySetWater(clamp(water + currentAdd, 0, 1000))} style={styles.logButton}></Button>
-                    </TouchableOpacity>
-
+        <>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.topBar} >
+                    <Text style={styles.topBarText}>Hydro<Text style={{ color: colors.secondary, fontStyle: "italic" }}>Mate</Text></Text>
                 </View>
-            </View>
+                <View style={styles.mainContent}>
+                    <View style={styles.progressSection} elevation={5}>
+                        <Text style={styles.headingText}>Progress</Text>
+                        <CircularProgressBase
+                            value={clamp(water / goal * 100, 0, 100)}
+                            radius={120}
+                            activeStrokeColor={colors.primary}
+                            inActiveStrokeColor={colors.primary}
+                            activeStrokeWidth={30}
+                            inActiveStrokeWidth={30}
+                            inActiveStrokeOpacity={0.2}
+                        >
+                            <Text style={styles.ProgressNumberText}>{water} oz / <Text style={{ opacity: 0.5 }}>{goal} oz</Text></Text>
+                        </CircularProgressBase>
+                        <View style={styles.amountToAddAdjustView}>
+                            <TouchableOpacity style={styles.addSubtractTouchable} onPress={() => setCurrentAdd(clamp(currentAdd - 0.5, 0.5, 100))} onLongPress={() => setCurrentAdd(clamp(currentAdd - 10, 0.5, 100))}>
+                                <Image style={styles.addSubtractIcon} source={require("../assets/images/minus.png")}></Image>
+                            </TouchableOpacity>
+                            <Text style={styles.currentAddText}>{currentAdd} oz</Text>
+                            <TouchableOpacity style={styles.addSubtractTouchable} onPress={() => setCurrentAdd(clamp(currentAdd + 0.5, 0.5, 100))} onLongPress={() => setCurrentAdd(clamp(currentAdd + 10, 0.5, 100))}>
+                                <Image style={styles.addSubtractIcon} source={require("../assets/images/plus.png")}></Image>
+                            </TouchableOpacity>
+                        </View>
 
-        </SafeAreaView>
+                        <Button title="LOG" onPress={() => mySetWater(clamp(water + currentAdd, 0, 1000))} style={styles.logButton}></Button>
+
+                    </View>
+                </View>
+
+            </SafeAreaView>
+            <LottieView
+                ref={confettiRef}
+                source={require("../assets/animations/confettiv2.json")}
+                autoPlay={true}
+                loop={false}
+                style={styles.lottie}
+                resizeMode='cover'
+                pointerEvents="none"
+                elevation={6}
+            />
+        </>
     );
 }
 
@@ -96,7 +118,10 @@ const styles = StyleSheet.create({
     addSubtractIcon: {
         width: 30,
         height: 30,
-        opacity: 0.5
+        opacity: 0.5,
+    },
+    addSubtractTouchable: {
+        zIndex: 1
     },
     amountToAddAdjustView: {
         width: '100%',
@@ -124,7 +149,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         justifyContent: 'flex-start',
         alignItems: 'center',
-        paddingTop: StatusBar.currentHeight
+        paddingTop: StatusBar.currentHeight,
     },
     headingText: {
         fontSize: 50,
@@ -135,15 +160,10 @@ const styles = StyleSheet.create({
     },
     logButton: {
         width: '15%',
-        alignSelf: "flex-start"
+        alignSelf: "flex-start",
     },
     lottie: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: 1000,
+        ...StyleSheet.absoluteFillObject,
         pointerEvents: 'none',
     },
     mainContent: {
--- app/utils/AsyncStorageManager.js
+++ app/utils/AsyncStorageManager.js
@@ -1,10 +1,21 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export function storeValue(key, value, callback) {
-    AsyncStorage.setItem(key, value, callback)
+export const storeData = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value)
+        console.log("set key " + key.toString() + " to value " + value.toString())
+    }
+    catch (err) {
+        console.warn(err)
+    }
 }
 
-export function getValue(key, callback) {
-    AsyncStorage.getItem(key)
-        .then(function(value) { callback(value) })
+export const getData = async (key) => {
+    try {
+        const value = await AsyncStorage.getItem(key)
+        if (value !== null) return value;
+        return null;
+    } catch (err) {
+        console.warn(err)
+    }
 }
